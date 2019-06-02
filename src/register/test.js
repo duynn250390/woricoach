baca9ae9c9c88d50b63e4bc0289ce260d9033ba5
woricoach <duynn250390@gmail.com>
2019-06-02 12:20:03 +0700

    test_go

---
+++ src/register/test.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+class test extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(test);
